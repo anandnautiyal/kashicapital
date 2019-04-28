fba18e9340e33c93bbb7e96256b1604776cfa984
Kundrai <30685139+kundrai@users.noreply.github.com>
2019-04-28 23:04:45 +0530

    Added additional Finder methods for Loan entity

--- src/main/java/com/kcfinance/loans/app/service/loan/impl/LoanService.java
+++ src/main/java/com/kcfinance/loans/app/service/loan/impl/LoanService.java
@@ -1,6 +1,7 @@
 package com.kcfinance.loans.app.service.loan.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kcfinance.loans.Exceptions.GenericException;
+import com.kcfinance.loans.app.modals.ApplicantBusinessDetail;
 import com.kcfinance.loans.app.modals.Loan;
 import com.kcfinance.loans.app.service.loan.ILoanService;
+import com.kcfinance.loans.dao.ApplicantBusinessDetailRepository;
 import com.kcfinance.loans.dao.LoanRepository;
 
 /**
@@ -26,6 +30,9 @@ public class LoanService implements ILoanService{
     @Autowired
     private LoanRepository loanRepository ;
     
+    @Autowired
+    private ApplicantBusinessDetailRepository applicantBusinessDetailRepository;
+    
 
 	@Override
 	public List<Loan> getAllLoans() {
@@ -37,4 +44,57 @@ public class LoanService implements ILoanService{
 			logger.debug("Loan records = {}",loanList);
 		return loanList;
 	}
+
+
+	@Override
+	public Optional<Loan> getById(Long ld) {
+		// TODO Auto-generated method stub
+		return loanRepository.findById(ld);
+	}
+
+
+	public Loan getByGstNumber(String gstNo) {
+		// TODO Auto-generated method stub
+		if(logger.isDebugEnabled())
+			logger.debug("getByGstNo start");
+		Loan loanDetail = null;
+		List<Loan> loans = getAllLoans();
+		for (Loan loan : loans) {
+			List<ApplicantBusinessDetail> applicantBusinessDetailList = loan.getApplicantBusinessDetails();
+			for (ApplicantBusinessDetail applicantBusinessDetail : applicantBusinessDetailList) {
+				String applicantGstNumber = applicantBusinessDetail.getGstNumber();
+				if(applicantGstNumber != null && applicantGstNumber.equalsIgnoreCase(gstNo)) {
+					loanDetail = loan;
+					if(logger.isDebugEnabled())
+						logger.debug("Loan by {} = {}", gstNo, loanDetail);
+					break;
+				}
+			}
+		}
+		return loanDetail;
+	}
+	
+	@Override
+	public Loan getByGstNo(String gstNo) {
+		// TODO Auto-generated method stub
+		if(logger.isDebugEnabled())
+			logger.debug("getByGstNo start");
+		ApplicantBusinessDetail applicantBusinessDetail = applicantBusinessDetailRepository.findByGstNumber(gstNo);
+		Loan loanDetail = null;
+		if(applicantBusinessDetail != null) {
+			loanDetail = applicantBusinessDetail.getLoan();
+			if(logger.isDebugEnabled())
+				logger.debug("Loan by {} = {}", gstNo, loanDetail);
+		}
+		else
+			throw new GenericException(Loan.NAME, gstNo);
+		return loanDetail;
+	}
+	
+/*	@Override
+    public Optional<Loan> findByCode(String code) {
+        return loanRepository.findByCode(code);
+    }*/
+	
+	
 }
